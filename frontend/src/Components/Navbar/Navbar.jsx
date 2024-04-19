cc551baa3f0946acf630ca27677794587e7d4b8a
brucekid96 <kazebrice98@gmail.com>
2024-04-19 18:47:45 +0200

    Implement responsive navbar with logo, menu, and cart icon

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [menu,setMenu] = useState("shop");
+
   return (
-    <div>
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>ShoesShop</p>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu === "shop" ? <hr /> : null}</li>
+        <li onClick={()=>{setMenu("mens")}}>Men{menu==="mens"? <hr /> : null}</li>
+        <li onClick={()=>{setMenu("womens")}}>Women{menu==="womens"? <hr /> : null}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"? <hr /> : null}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt=""  />
+      <div className="nav-cart-count">
+        0
+      </div>
+      </div>
       
     </div>
   )
