bb58dbf2bacd12bf84532fc36d8b3cecba401454
brucekid96 <kazebrice98@gmail.com>
2024-05-09 16:09:41 +0200

    created the context of the App which is ShopContext and called the context to us it in ShopCategory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,9 @@ import Loginsignup from './Pages/Loginsignup';
 import Shop from './Pages/Shop';
 import Cart from './Pages/Cart';
 import Footer from './Components/Footer/Footer.jsx';
+import men_banner from './Components/Assets/men_banner.jpg'
+import women_banner from './Components/Assets/women_banner.jpg'
+import kid_banner from './Components/Assets/kids_banner.jpg'
 
 function App() {
   return (
@@ -16,9 +19,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
-        <Route path='/mens' element={<ShopCategory category="men"/>}/>
-        <Route path='/womens' element={<ShopCategory category="women"/>}/>
-        <Route path='/kids' element={<ShopCategory category="kid"/>}/>
+        <Route path='/mens' element={<ShopCategory banner= {men_banner} category="men"/>}/>
+        <Route path='/womens' element={<ShopCategory banner= {women_banner} category="women"/>}/>
+        <Route path='/kids' element={<ShopCategory banner= {kid_banner} category="kid"/>}/>
         <Route path="/product" element={<Product/>}> 
         <Route path= ':productId' element={<Product/>}/>
         </Route>
---
+++ frontend/src/Components/Assets/all_product.js
@@ -0,0 +1,84 @@
+import p1_img from './heroo_image.jpeg'
+import p2_img from './air_max_infrared.jpeg'
+import p3_img from './air_nike.jpg'
+import p4_img from './mens_formal_shoes.webp'
+import p5_img from './kubistar.jpg'
+import p6_img from './haut_talon.png'
+
+
+
+
+
+let all_product = [
+    {
+        id:1,
+        name:"Ponci",
+        category:"men",
+        image:p1_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:2,
+        name:"Air Max Infrared",
+        category:"Men",
+        image:p2_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:3,
+        name:"Air Nike",
+        category:"men",
+        image:p3_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:4,
+        name:"Mens Formal Shoes",
+        category:"men",
+        image:p4_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:5,
+        name:"Mens Formal Shoes",
+        category:"men",
+        image:p5_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:6,
+        name:"Haut Talon",
+        category:"women",
+        image:p6_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:7,
+        name:"Kubistar",
+        category:"kid",
+        image:p5_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+    {
+        id:8,
+        name:"Air Nike",
+        category:"men",
+        image:p3_img,
+        new_price:100.00,
+        old_price:120.50,
+    },
+
+ 
+
+   
+
+]
+
+export default all_product;
